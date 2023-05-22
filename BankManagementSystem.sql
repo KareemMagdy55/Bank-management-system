502202ca36a3dc06313b1601f20f891c17d335b2
Yaseen Emad <118124426+YaseenEmad@users.noreply.github.com>
2023-05-22 23:14:06 +0300

    Update BankManagementSystem.sql
    Changed the order of tables, It's the final version for the project now.

--- BankManagementSystem.sql
+++ BankManagementSystem.sql
@@ -1,87 +1,83 @@
 use master
-Drop database BankManagementSystem
+DROP DATABASE BankManagementSystem
 CREATE DATABASE BankManagementSystem
- 
-CREATE TABLE Bank (
-	Name varchar(15),
-	Code varchar(10),
-	Address varchar(50),
 
-	CONSTRAINT Bank_pk PRIMARY KEY (Code),
-	CONSTRAINT Bank_uq Unique (Name)
+CREATE TABLE Bank (
+    Name varchar(15),
+    Code varchar(10),
+    Address varchar(50),
+
+    CONSTRAINT Bank_pk PRIMARY KEY (Code),
+    CONSTRAINT Bank_uq UNIQUE (Name)
 );
 
 CREATE TABLE Branch (
-	Number varchar(10),
-	Address varchar(50),
-	BankCode varchar(10), 
+    Number varchar(10),
+    Address varchar(50),
+    BankCode varchar(10),
 
-	CONSTRAINT Branch_pk PRIMARY KEY (Number),
-	CONSTRAINT Branch_fk foreign key (BankCode) references Bank(Code)
-
-);
-
-CREATE TABLE Customer (
-	Name varchar(100) NOT NULL,
-	SSN varchar(10),
-	Address varchar(50),
-	Phone varchar(12),
-	Password varchar(15) NOT NULL, 
-	BankCode varchar(10), 
-	BranchCode varchar(10),
-	AccountNumber varchar(17),
-  
-	CONSTRAINT Customer_pk PRIMARY KEY (SSN),
-	CONSTRAINT CustomerBranch_fk foreign key (BranchCode) references Branch(Number), 
-	CONSTRAINT CustomerBank_fk foreign key (BankCode) references Bank(Code),
-	CONSTRAINT CustomerAccNum_fk foreign key (AccountNumber) references Account(Number), 
-	CONSTRAINT Customer_uq Unique (Phone)
+    CONSTRAINT Branch_pk PRIMARY KEY (Number),
+    CONSTRAINT Branch_fk FOREIGN KEY (BankCode) REFERENCES Bank(Code)
 );
 
 CREATE TABLE Employee (
-	Name varchar(15),
-	SSN varchar(10),
-	Password varchar(15),
-	AccessLevel int,
-	BankCode varchar(10), 
-	BranchCode varchar(10),
+    Name varchar(15),
+    SSN varchar(10),
+    Password varchar(15),
+    AccessLevel int,
+    BankCode varchar(10),
+    BranchCode varchar(10),
 
-	CONSTRAINT EmployeeBranch_fk foreign key (BranchCode) references Branch(Number), 
-	CONSTRAINT EmployeeBank_fk foreign key (BankCode) references Bank(Code), 
-	CONSTRAINT Employee_pk PRIMARY KEY (SSN)
+    CONSTRAINT EmployeeBranch_fk FOREIGN KEY (BranchCode) REFERENCES Branch(Number),
+    CONSTRAINT EmployeeBank_fk FOREIGN KEY (BankCode) REFERENCES Bank(Code),
+    CONSTRAINT Employee_pk PRIMARY KEY (SSN)
 );
 
 CREATE TABLE Admin (
-	Name varchar(15),
-	SSN varchar(10),
-	Password varchar(15),
-	
-	CONSTRAINT Admin_PK PRIMARY KEY (SSN)
-	);
-	
-CREATE TABLE Account (
-	 AccountType varchar(15),
-	 Number varchar(17),
-	 Balance float,
-	 CustomerSSN varchar(10), 
-	 EmployeeID varchar(10), 
+    Name varchar(15),
+    SSN varchar(10),
+    Password varchar(15),
 
-	 CONSTRAINT AccountCustomer_FK Foreign KEY (CustomerSSN) references Customer(SSN), 
-	 CONSTRAINT AccountEmp_FK Foreign KEY (EmployeeID) references Employee(SSN),
-	 CONSTRAINT Account_pk PRIMARY KEY (Number)
+    CONSTRAINT Admin_PK PRIMARY KEY (SSN)
 );
 
+CREATE TABLE Account (
+    AccountType varchar(15),
+    Number varchar(17),
+    Balance float,
+    CustomerSSN varchar(10),
+    EmployeeID varchar(10),
 
+    CONSTRAINT Account_pk PRIMARY KEY (Number),
+    CONSTRAINT AccountCustomer_FK FOREIGN KEY (CustomerSSN) REFERENCES Customer(SSN),
+    CONSTRAINT AccountEmp_FK FOREIGN KEY (EmployeeID) REFERENCES Employee(SSN)
+);
+
+CREATE TABLE Customer (
+    Name varchar(100) NOT NULL,
+    SSN varchar(10),
+    Address varchar(50),
+    Phone varchar(12),
+    Password varchar(15) NOT NULL,
+    BankCode varchar(10),
+    BranchCode varchar(10),
+    AccountNumber varchar(17),
+
+    CONSTRAINT Customer_pk PRIMARY KEY (SSN),
+    CONSTRAINT CustomerBranch_fk FOREIGN KEY (BranchCode) REFERENCES Branch(Number),
+    CONSTRAINT CustomerBank_fk FOREIGN KEY (BankCode) REFERENCES Bank(Code),
+    CONSTRAINT Customer_uq UNIQUE (Phone)
+);
 
 CREATE TABLE Loan (
-	Type varchar(15),
-	Number int,
-	Balance float,
-	Status varchar(15),
-	EmployeeSSN varchar(10), 
-	CustomerSSN varchar(10),
+    Type varchar(15),
+    Number int,
+    Balance float,
+    Status varchar(15),
+    EmployeeSSN varchar(10),
+    CustomerSSN varchar(10),
 
-	CONSTRAINT LoanEmp_FK FOREIGN KEY (EmployeeSSN) references Employee(SSN),
-	CONSTRAINT LoanCustomer_FK FOREIGN KEY (CustomerSSN) references Customer (SSN), 
-	CONSTRAINT Loan_pk PRIMARY KEY (Number) 
+    CONSTRAINT LoanEmp_FK FOREIGN KEY (EmployeeSSN) REFERENCES Employee(SSN),
+    CONSTRAINT LoanCustomer_FK FOREIGN KEY (CustomerSSN) REFERENCES Customer (SSN),
+    CONSTRAINT Loan_pk PRIMARY KEY (Number)
 );
